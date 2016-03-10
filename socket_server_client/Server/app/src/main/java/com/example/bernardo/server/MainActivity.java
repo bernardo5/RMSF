53c2f1276c714f42de4d70848e469d70196447f6
bernardo5 <bdnabaisgomes@yahoo.com>
2016-03-10 22:34:16 +0000

    android client-server

---
+++ socket_server_client/Server/app/src/main/java/com/example/bernardo/server/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.bernardo.server;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    Server server;
+    TextView infoip, msg;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        infoip = (TextView) findViewById(R.id.infoip);
+        msg = (TextView) findViewById(R.id.msg);
+        server = new Server(this);
+        infoip.setText(server.getIpAddress()+":"+server.getPort());
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        server.onDestroy();
+    }
+
+
+}
