c7cb24406a819ca55fc5bc55b423b5e6f92ccb16
bernardo5 <bdnabaisgomes@yahoo.com>
2016-03-21 23:55:23 +0000

    a criar login

---
+++ app/SigFox/app/src/main/java/com/example/eu/sigfox/CreateLogActivity.java
@@ -0,0 +1,40 @@
+package com.example.eu.sigfox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CreateLogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_log);
+    }
+
+    public void createLog(View view) {
+        EditText Name = (EditText) findViewById(R.id.Name);
+        EditText Pass = (EditText) findViewById(R.id.password);
+        EditText Device_id = (EditText) findViewById(R.id.device_id);
+
+        try {
+            FileOutputStream fileOutputStream = openFileOutput("logs.txt", MODE_PRIVATE); //no other app can open file
+            fileOutputStream.write(Name.getText().toString().getBytes());
+            fileOutputStream.write(Pass.getText().toString().getBytes());
+            fileOutputStream.write(Device_id.getText().toString().getBytes());
+            fileOutputStream.close();
+            Toast.makeText(getApplicationContext(), "Log successfully created", Toast.LENGTH_LONG).show();
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
--- app/SigFox/app/src/main/java/com/example/eu/sigfox/MainActivity.java
+++ app/SigFox/app/src/main/java/com/example/eu/sigfox/MainActivity.java
@@ -3,21 +3,34 @@ package com.example.eu.sigfox;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Button;
 import android.content.Intent;
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void nextPage(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        Button button = (Button)view;
-        String message = button.getText().toString();
-        startActivity(intent);
+    public void checkLogin(View view) {
+        File file = new File(getFilesDir(), "logs.txt");
+        if(file.exists()){
+            //user is logged in
+            Intent logged = new Intent(this, LogsActivity.class);
+            startActivity(logged);
+        }else{
+            //user has to create a log
+            Intent create_log = new Intent(this, CreateLogActivity.class);
+            startActivity(create_log);
+        }
+
+
     }
 }
