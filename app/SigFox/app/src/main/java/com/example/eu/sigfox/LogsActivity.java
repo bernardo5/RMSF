ecbe3224c45cfc099498e5abd82202b3b02e1ec3
bernardo5 <bdnabaisgomes@yahoo.com>
2016-05-01 13:10:24 +0100

    alarm

--- app/SigFox/app/src/main/java/com/example/eu/sigfox/LogsActivity.java
+++ app/SigFox/app/src/main/java/com/example/eu/sigfox/LogsActivity.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Handler;
@@ -240,7 +241,7 @@ public class LogsActivity extends AppCompatActivity {
         new_device.putExtra("username", UsernameApp);
         startActivity(new_device);
     }
-    @TargetApi(11)
+    @TargetApi(16)
     public class JSONTask extends AsyncTask<String, String, String> {
 
         @Override
@@ -333,7 +334,7 @@ public class LogsActivity extends AppCompatActivity {
 
 
             messageTime=part2;
-           //
+           //condicao alarme
             Notification noti=new Notification.Builder(getBaseContext())
                     .setTicker("Threshold")
                     .setContentTitle("Threshold ultrapassed")
@@ -341,9 +342,8 @@ public class LogsActivity extends AppCompatActivity {
                     .setSmallIcon(R.drawable.ic_alert)
                     .setContentIntent(pIntent).getNotification();
             NotificationManager notificationManager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            noti.flags = Notification.DEFAULT_LIGHTS | Notification.FLAG_AUTO_CANCEL;
             notificationManager.notify(0, noti);
-
-
 
         }
     }
