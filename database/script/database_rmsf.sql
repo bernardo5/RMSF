7779a645085aa5d054947ede5a07216dd2e102a8
bernardo5 <bdnabaisgomes@yahoo.com>
2016-05-02 12:02:07 +0100

    a funcionar

--- database/script/database_rmsf.sql
+++ database/script/database_rmsf.sql
@@ -23,5 +23,7 @@ create table usersAlarms(
 insert into users values('Bernardo', '56c47b4c9336adb5ba39c9b6', 'dd6bd147da1dcc9e34b4674b0f0be948', '0');
 insert into usersDevices values('Bernardo', '56bdd1da9336b182b106d3b0');
 insert into usersDevices values('Bernardo', 'teste');
+insert into users values('Diogo', '571a89a093368471a5a2e879','e5c2bfa6b523d9028d666f9f1f6ae4de', '0');
+insert into usersDevices values('Diogo', '571a18ca9336f164577acaa9');
 
 
